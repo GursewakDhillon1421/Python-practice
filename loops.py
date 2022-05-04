07db6ca709ce81cca9083741728ecfbaa4f8b4e0
gursewakdhillon1421 <gursewakdhillon0241@gmail.com>
2022-05-04 15:28:05 +0530

    04-05-22

---
+++ loops.py
@@ -0,0 +1,13 @@
+# For loop
+i = 0
+for i in range (i , 10):
+    print(i)
+
+my_set = {"my" , "name" , "is" , "gursewak", "singh"}
+for x in my_set:
+    print(x) 
+
+n = 10
+while(n):
+    print(n)
+    n = n - 1
